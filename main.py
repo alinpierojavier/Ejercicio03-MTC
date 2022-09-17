313dbe2b43aa54c44d94374aeb9a9a47eb6a6d1a
Piero <32024150+alinpierojavier@users.noreply.github.com>
2022-09-17 15:43:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Escriba un programa que solicite el número de filas e imprima un triángulo.
+
+print("**********************")
+n=int(input("Ingresar un número cualquiera: \n"))
+x=0
+while x<=n:
+  print("*"*x)
+  x=x+1
+ 
